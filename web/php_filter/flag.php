9c7ab4bc243699072ab39bfc2884c5edf19ede33
Eunho <eunhokim98@naver.com>
2022-12-30 15:03:44 +0900

    php_filtering_완성

--- web/php_filter/flag.php
+++ web/php_filter/flag.php
@@ -1,3 +1,9 @@
 <?php
+    #세션 검증을 통한 접근제어
+    session_start();  
+    if(!isset($_SESSION['logged_in'])){
+        echo "<script>alert('Permission Denied');</script>";
+        echo "<script>history.back();</script>"; 
+    }  
     echo "ISLAB{Web_hacking_is_fun!}";
 ?>
--- web/php_filter/login.php
+++ web/php_filter/login.php
@@ -1,21 +1,21 @@
 <?php
-
+//아이디를 prag_match를 통해 막았을 때, admin을 hex로 넣으면 성공
 // Check if the form has been submitted
 if (isset($_POST['submit'])) {
   // Get the username and password from the form
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  if(preg_match("/\*|\/|=|admin|-|#|;/i",$_GET['username'])) exit("no hack");
+  if(preg_match("/\*|\/|=|admin|-|#|;/i",$_POST['username'])) exit("no hack");
 
   // Check if the username and password are correct
-  if ($username == 'admin1' && $password == 'admin') {
+  if ($username == ' 0x61646d696e' && $password == 'admin') {
     // If the username and password are correct, set a session variable
     session_start();
     $_SESSION['logged_in'] = true;
 
     // Redirect the user to the protected page
-    header('Location: ../flag.php');
+    header('Location: ./flag.php');
     exit;
   } else {
     // If the username and password are incorrect, show an error message
