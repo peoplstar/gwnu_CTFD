655ae192c75c7a43377d26a032b2a8c6cb150e80
Eunho <eunhokim98@naver.com>
2022-12-30 14:41:57 +0900

    php_filter_수정2

--- web/php_filter/easy_php.php
+++ web/php_filter/easy_php.php
@@ -1,23 +1,44 @@
+<?php
+
+// Check if the form has been submitted
+if (isset($_POST['submit'])) {
+  // Get the username and password from the form
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if(preg_match("/\*|\/|=|admin|-|#|;/i",$_GET['username'])) exit("no hack");
+
+  // Check if the username and password are correct
+  if ($username == 'admin1' && $password == 'admin') {
+    // If the username and password are correct, set a session variable
+    session_start();
+    $_SESSION['logged_in'] = true;
+
+    // Redirect the user to the protected page
+    header('Location: ../flag.php');
+    exit;
+  } else {
+    // If the username and password are incorrect, show an error message
+    $error = 'Invalid username or password';
+  }
+}
+
+?>
+<!DOCTYPE html>
 <html>
 <head>
-<title>easy_php</title>
-<style>
-  table {
-    width: 100%;
-    border-top: 1px solid #444444;
-    border-collapse: collapse;
-  }
-  th, td {
-    border-bottom: 1px solid #444444;
-    padding: 10px;
-  }
-</style>
+  <title>Administrator Login</title>
 </head>
 <body>
-<table border=1 align=center width=300>
-<tr><td width=50>no</td><td>subject</td><td>file_root</td></tr>
-<tr><td>2</td><td>cat_img</td><td> cat.jpg[<a href="./cat.jpg" download="cat.jpg">download</a>]</tr>
-<tr><td>1</td><td>flag</td><td>flag.docx [<a href=javascript:alert("Permission_Denied")>download</a>]</td></tr>
-</table>
+  <?php if (isset($error)): ?>
+    <p style="color: red;"><?php echo $error; ?></p>
+  <?php endif; ?>
+  <form method="post">
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Submit" name="submit">
+  </form> 
 </body>
 </html>
---
+++ web/php_filter/flag.php
@@ -0,0 +1,3 @@
+<?php
+    echo "ISLAB{Web_hacking_is_fun!}";
+?>
